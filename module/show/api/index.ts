f499cc2bc211bd5248c181b7d0888cbcd10a9fe5
Antoine CHEVALIER <53263615+SeyBoo@users.noreply.github.com>
2022-12-21 09:23:28 +0100

    import based on env value

--- module/show/api/index.ts
+++ module/show/api/index.ts
@@ -10,7 +10,7 @@ let showBackendInstance: ShowBackend | undefined;
 
 export async function getShowsBackend(): Promise<ShowBackend> {
   if (showBackendInstance === undefined) {
-    const mod = await import("./backends/" + "dummy");
+    const mod = await import("./backends/" + process.env.NEXT_PUBLIC_SHOWS_BACKEND);
     showBackendInstance = new mod.default() as ShowBackend;
   }
   return showBackendInstance;
