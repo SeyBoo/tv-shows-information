85a3fb6042e9f1974c8c5bc9abbf42ebd2ba532d
Antoine CHEVALIER <53263615+SeyBoo@users.noreply.github.com>
2022-12-22 16:14:26 +0100

    ShowBackend imdb

---
+++ module/show/api/backends/imdb/index.ts
@@ -0,0 +1,26 @@
+import { ShowBackend } from "../..";
+import { getFromApi } from "../../../../../common/api/config";
+import { ShowI } from "../../../types/show.interface";
+import { ShowDetailsReponse, ShowResponse } from "./schema";
+
+export default class DummyShows implements ShowBackend {
+  formatShow(show: ShowResponse): ShowI {
+    const imagePathUrl = "http://image.tmdb.org/t/p/w500";
+
+    return {
+      id: show.id.toString(),
+      image: imagePathUrl + show.poster_path,
+      title: show.name,
+    };
+  }
+
+  async fetchPopularShows(): Promise<ShowI[]> {
+    const data: ShowDetailsReponse = await getFromApi("discover/tv");
+
+    const formatedShows: ShowI[] = await Promise.all(
+      data.results.map((show) => this.formatShow(show))
+    );
+
+    return formatedShows;
+  }
+}
---
+++ module/show/api/backends/imdb/schema.ts
@@ -0,0 +1,20 @@
+export interface ShowDetailsReponse {
+  pages: number;
+  results: ShowResponse[];
+}
+
+export interface ShowResponse {
+  backdrop_path: string | null;
+  first_air_date: string;
+  genre_ids: number[];
+  id: number;
+  name: string;
+  origin_country: string[];
+  original_language: string;
+  original_name: string;
+  overview: string;
+  popularity: number;
+  poster_path: string;
+  vote_average: number;
+  vote_count: number;
+}
