ce71da8cdf4053c802c37b8e4dffb4e5f7d92ae8
Antoine CHEVALIER <53263615+SeyBoo@users.noreply.github.com>
2022-12-22 21:06:52 +0100

    episodes backend moviedb

---
+++ module/episode/api/backends/moviedb/index.ts
@@ -0,0 +1,34 @@
+import { EpisodesBackend } from "../..";
+import { getFromApi } from "../../../../../common/api/config";
+import { Episode, FetchEpisodesProps } from "../../../types/episode.interface";
+import { EpisodeApiResponse, EpisodesApiResponse } from "./schema";
+
+export default class MovieDBEpisodesBackend implements EpisodesBackend {
+  async formatSeason(episode: EpisodeApiResponse): Promise<Episode> {
+    const imagePathUrl = "http://image.tmdb.org/t/p/w500";
+
+    return {
+      id: episode.id,
+      image: imagePathUrl + episode.still_path,
+      title: episode.name,
+      episodeNumber: episode.episode_number,
+      overview: episode.overview,
+      runtime: episode.runtime,
+    };
+  }
+
+  async fetchEpisodes({
+    movieId,
+    seasonNumber,
+  }: FetchEpisodesProps): Promise<Episode[]> {
+    const data: EpisodesApiResponse = await getFromApi(
+      `tv/${movieId}/season/${seasonNumber}`
+    );
+
+    const formatedSeasons: Episode[] = await Promise.all(
+      data.episodes.map((episode) => this.formatSeason(episode))
+    );
+
+    return formatedSeasons;
+  }
+}
---
+++ module/episode/api/backends/moviedb/schema.ts
@@ -0,0 +1,55 @@
+interface CrewMember {
+  job: string;
+  department: string;
+  credit_id: string;
+  adult: boolean;
+  gender: number;
+  id: number;
+  known_for_department: string;
+  name: string;
+  original_name: string;
+  popularity: number;
+  profile_path: string;
+}
+
+interface Guest {
+  character: string;
+  credit_id: string;
+  order: number;
+  adult: boolean;
+  gender: number;
+  id: number;
+  known_for_department: string;
+  name: string;
+  original_name: string;
+  popularity: number;
+  profile_path: string;
+}
+
+export interface EpisodeApiResponse {
+  air_date: string;
+  episode_number: number;
+  id: number;
+  name: string;
+  overview: string;
+  production_code: string;
+  runtime: number;
+  season_number: number;
+  show_id: number;
+  still_path: string;
+  vote_average: number;
+  vote_count: number;
+  crew: CrewMember[];
+  guest_stars: Guest[];
+}
+
+export interface EpisodesApiResponse {
+  _id: string;
+  air_date: string;
+  episodes: EpisodeApiResponse[];
+  name: string;
+  overview: string;
+  id: number;
+  poster_path: string;
+  season_number: number;
+}
