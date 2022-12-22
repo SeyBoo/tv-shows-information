febd04e8c5f9bba3dd2e9a29983dc9595181e3a9
Antoine CHEVALIER <53263615+SeyBoo@users.noreply.github.com>
2022-12-22 21:07:31 +0100

    episodeCard

---
+++ module/episode/components/episodeCard.tsx
@@ -0,0 +1,40 @@
+import { FunctionComponent } from "react";
+import { Episode } from "../types/episode.interface";
+import Image from "next/image";
+import { useShow } from "../../../common/hooks/useShow";
+
+interface EpisodeCardProps {
+  episode: Episode;
+}
+
+export const EpisodeCard: FunctionComponent<EpisodeCardProps> = ({
+  episode,
+}) => {
+  const { setSelectedScreen, setSelectedEpisode } = useShow();
+
+  const handleClick = () => {
+    setSelectedScreen("episode");
+    setSelectedEpisode(episode.episodeNumber);
+  };
+
+  return (
+    <button
+      className="relative w-[300px] h-[400px] rounded-2xl hover:border-8 hover:border-green-200"
+      onClick={() => handleClick()}
+    >
+      <Image
+        src={episode.image}
+        alt={`${episode.title} illustration`}
+        fill
+        sizes="1"
+        className="rounded-lg"
+      />
+      <div className="text-white absolute p-2 w-full bottom-0">
+        <div className="text-xl font-bold flex justify-between">
+          <p>{episode.title}</p>
+          <p>{episode.episodeNumber}</p>
+        </div>
+      </div>
+    </button>
+  );
+};
