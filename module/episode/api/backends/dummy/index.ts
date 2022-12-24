2fdf8f84462643ce393413d2f8d99a3124edce72
Antoine CHEVALIER <53263615+SeyBoo@users.noreply.github.com>
2022-12-24 14:37:22 +0100

    dummy fetchEpisode

--- module/episode/api/backends/dummy/index.ts
+++ module/episode/api/backends/dummy/index.ts
@@ -1,5 +1,9 @@
-import { FetchEpisodesProps, Episode } from "../../../types/episode.interface";
 import { EpisodesBackend } from "..";
+import {
+  FetchEpisodesProps,
+  Episode,
+  FetchEpisodeProps,
+} from "../../../types/episode.interface";
 import { DummyEpisodesData } from "./dummy-data";
 
 export default class DummyEpisodesBackend implements EpisodesBackend {
@@ -8,5 +12,13 @@ export default class DummyEpisodesBackend implements EpisodesBackend {
     seasonNumber,
   }: FetchEpisodesProps): Promise<Episode[]> {
     return DummyEpisodesData;
+  }
+
+  async fetchEpisode({
+    movieId,
+    seasonNumber,
+    episodeNumber,
+  }: FetchEpisodeProps): Promise<Episode> {
+    return DummyEpisodesData[episodeNumber];
   }
 }
