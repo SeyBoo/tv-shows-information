8ba711e137584bf295cc92ea5abf9404bae923a4
Antoine CHEVALIER <53263615+SeyBoo@users.noreply.github.com>
2022-12-24 16:46:04 +0100

    updat nav bar

--- common/components/nav.tsx
+++ common/components/nav.tsx
@@ -3,36 +3,56 @@ import { useShow } from "../hooks/useShow";
 
 export const Nav: FunctionComponent = () => {
   const {
+    selectedShow,
+    selectedSeasonNumber,
+    selectedEpisodeNumber,
     selectedScreen,
     setSelectedScreen,
     setSelectedSeasonNumber,
     setSelectedShow,
+    setSelectedEpisodeNumber,
   } = useShow();
 
-  const handleGoBack = () => {
-    switch (selectedScreen) {
-      case "episode":
-        setSelectedScreen("episodes");
-        break;
-      case "episodes":
-        setSelectedScreen("seasons");
-        setSelectedSeasonNumber(0);
-        break;
-      case "seasons":
-        setSelectedScreen("shows");
-        setSelectedShow("");
-        break;
-      default:
-        break;
-    }
+  const handleGoToEpisodes = () => {
+    setSelectedScreen("episodes");
+    setSelectedEpisodeNumber(0);
+  };
+
+  const handleGoToSeasons = () => {
+    setSelectedScreen("seasons");
+    setSelectedSeasonNumber(0);
+    setSelectedEpisodeNumber(0);
+  };
+
+  const handleGoToShows = () => {
+    setSelectedScreen("shows");
+    setSelectedSeasonNumber(0);
+    setSelectedShow({
+      id: 0,
+      title: "",
+    });
   };
 
   return (
     <nav className="pt-8 px-8">
       {selectedScreen !== "shows" && (
-        <button onClick={() => handleGoBack()} className="text-3xl text-white">
-          Go Back
-        </button>
+        <div className="flex text-white gap-2 text-xl">
+          {selectedShow.id !== 0 && (
+            <button onClick={() => handleGoToShows()}>
+              {selectedShow.title}
+            </button>
+          )}
+          {selectedSeasonNumber !== 0 && (
+            <button onClick={() => handleGoToSeasons()}>
+              - Season {selectedSeasonNumber}
+            </button>
+          )}
+          {selectedEpisodeNumber !== 0 && (
+            <button onClick={() => handleGoToEpisodes()}>
+              - Episode {selectedEpisodeNumber}
+            </button>
+          )}
+        </div>
       )}
     </nav>
   );
