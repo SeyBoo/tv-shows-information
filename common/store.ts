a69b9c1f245d9217782877f8c3810f21b88343f2
Antoine CHEVALIER <53263615+SeyBoo@users.noreply.github.com>
2022-12-24 12:35:30 +0100

    remove seasons store

--- common/store.ts
+++ common/store.ts
@@ -1,12 +1,10 @@
 import { Action, configureStore, ThunkAction } from "@reduxjs/toolkit";
 import { episodesSlice } from "../module/episode/state/slice";
-import { seasonsSlice } from "../module/seasons/store/slice";
 import { showSlice } from "../module/show/store/slice";
 
 const store = configureStore({
   reducer: {
     [showSlice.name]: showSlice.reducer,
-    [seasonsSlice.name]: seasonsSlice.reducer,
     [episodesSlice.name]: episodesSlice.reducer,
   },
 });
--- module/seasons/components/src/seasonsList.tsx
+++ module/seasons/components/src/seasonsList.tsx
@@ -2,32 +2,16 @@ import { FromTheTopAnimation } from "../../../../common/animations";
 import { useShow } from "../../../../common/hooks/useShow";
 import { SeasonCard } from "./seasonCard";
 import calculateDelay from "../../../../common/utils/calculateAnimationDelay";
-import { useAppDispatch, useAppSelector } from "../../../../common/hooks/store";
 import { Grid } from "../../../../common/components/grid";
-import { useCallback, useEffect } from "react";
-import { fetchSeasons } from "../../store/thunk";
+import { useGetSeasons } from "../../api/seasons.api";
 
 export const SeasonList = () => {
-  const seasons = useAppSelector((state) => state.seasons.seasons);
   const { selectedShow } = useShow();
-
-  const dispatch = useAppDispatch();
-
-  const handleFetchSeasons = useCallback(async () => {
-    try {
-      await dispatch(fetchSeasons(selectedShow));
-    } catch (e) {
-      console.log(e);
-    }
-  }, [dispatch, selectedShow]);
-
-  useEffect(() => {
-    handleFetchSeasons();
-  }, [handleFetchSeasons]);
+  const { data } = useGetSeasons(selectedShow);
 
   return (
     <Grid>
-      {seasons?.map((season, index) => (
+      {data?.map((season, index) => (
         <FromTheTopAnimation delay={calculateDelay(index)} key={index}>
           <SeasonCard season={season} />
         </FromTheTopAnimation>
