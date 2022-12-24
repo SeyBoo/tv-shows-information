bfcab24334c9c1bf2f4be58b65412fb6e49c7439
Antoine CHEVALIER <53263615+SeyBoo@users.noreply.github.com>
2022-12-24 14:38:38 +0100

    create getEpisode

--- module/episode/api/episode.api.ts
+++ module/episode/api/episode.api.ts
@@ -1,6 +1,7 @@
 import { useQuery, UseQueryResult } from "react-query";
 import {
   Episode,
+  FetchEpisodeProps,
   FetchEpisodesProps,
 } from "../types/episode.interface";
 import { getEpisodesBackend } from "./backends";
@@ -19,3 +20,20 @@ export const useGetEpisodes = ({
   return useQuery(EpisodeApiRoutes.GET_EPISODES, getEpisodes);
 };
 
+export const useGetEpisode = ({
+  movieId,
+  seasonNumber,
+  episodeNumber,
+}: FetchEpisodeProps): UseQueryResult<Episode> => {
+  const getEpisode = async (): Promise<Episode> => {
+    const episodesBackend = await getEpisodesBackend();
+    const data = await episodesBackend.fetchEpisode({
+      movieId,
+      seasonNumber,
+      episodeNumber,
+    });
+    return data;
+  };
+
+  return useQuery(EpisodeApiRoutes.GET_EPISODE, getEpisode);
+};
--- module/episode/api/episode.enum.ts
+++ module/episode/api/episode.enum.ts
@@ -1,3 +1,4 @@
 export enum EpisodeApiRoutes {
   GET_EPISODES = "GET_EPISODES",
+  GET_EPISODE = "GET_EPISODE",
 }
