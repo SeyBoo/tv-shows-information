a93cac9568f88df5146e7875f50da1fb70ddc1c4
Antoine CHEVALIER <53263615+SeyBoo@users.noreply.github.com>
2022-12-25 11:50:20 +0100

    add skeletons

--- common/components/index.ts
+++ common/components/index.ts
@@ -1,2 +1,4 @@
 export * from "./src/grid";
+export * from "./src/skeletons/imageSkeleton";
+export * from "./src/skeletons/textSkeleton";
 export * from "./src/nav";
---
+++ common/components/src/skeletons/textSkeleton.tsx
@@ -0,0 +1,5 @@
+import { FunctionComponent } from "react";
+
+export const TextSkeleton: FunctionComponent = () => {
+  return <div className="h-2.5 bg-gray-700 rounded-full mt-2"></div>;
+};
