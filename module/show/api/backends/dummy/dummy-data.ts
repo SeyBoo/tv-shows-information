dd57d47e6ef8834cbb6f3a9277525fa3289820fa
Antoine CHEVALIER <53263615+SeyBoo@users.noreply.github.com>
2022-12-25 11:51:33 +0100

    correct wrong data type

--- module/show/api/backends/dummy/dummy-data.ts
+++ module/show/api/backends/dummy/dummy-data.ts
@@ -2,31 +2,31 @@ import { ShowI } from "../../../types/show.interface";
 
 const DummyShowsData: ShowI[] = [
   {
-    id: "uuid-0",
+    id: 0,
     image:
       "https://www.themoviedb.org/t/p/w220_and_h330_face/jRXYjXNq0Cs2TcJjLkki24MLp7u.jpg",
     title: "avatar",
   },
   {
-    id: "uuid-1",
+    id: 1,
     image:
       "https://www.themoviedb.org/t/p/w220_and_h330_face/uAeZI1JJbLPq7Bu5dziH7emHeu7.jpg",
     title: "lost bullet 2",
   },
   {
-    id: "uuid-3",
+    id: 3,
     image:
       "https://www.themoviedb.org/t/p/w220_and_h330_face/peNC0eyc3TQJa6x4TdKcBPNP4t0.jpg",
     title: "Yellow stone",
   },
   {
-    id: "uuid-4",
+    id: 4,
     image:
       "https://www.themoviedb.org/t/p/w220_and_h330_face/umSVjVdbVwtx5ryCA2QXL44Durm.jpg",
     title: "heat",
   },
   {
-    id: "uuid-5",
+    id: 5,
     image:
       "https://www.themoviedb.org/t/p/w220_and_h330_face/kGmpIkjVRWHQ97AiEXC0pLt5VtY.jpg",
     title: "a christmas carol",
